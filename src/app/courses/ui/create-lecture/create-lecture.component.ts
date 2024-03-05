7069b5acf1ffaf3b1ae901091a9b442ff4c4e51d
Rocard Fonji <fonkengrocard@gmail.com>
2024-03-05 21:30:47 +0100

    (feat: create-course-lecture): implement function to save courses lectire data on firestore

--- src/app/courses/ui/create-lecture/create-lecture.component.ts
+++ src/app/courses/ui/create-lecture/create-lecture.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Course } from '../../interface/types';
+import { Course, Lecture } from '../../interface/types';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-create-lecture',
@@ -14,6 +15,7 @@ export class CreateLectureComponent {
   fb = inject(FormBuilder);
   @Input() course!: Course;
   @Output() formSubmit = new EventEmitter<void>();
+  firestore = inject(Firestore);
 
   constructor() {
     this.lectureForm = this.fb.group({
@@ -23,5 +25,17 @@ export class CreateLectureComponent {
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    const lecture: Lecture = {
+      ... this.lectureForm.value,
+      id: self.crypto.randomUUID()
+    };
+    const docRef = doc(this.firestore, `courses/${this.course.id}/lectures/${lecture.id}`);
+    setDoc(docRef, lecture).then(() => {
+      this.formSubmit.emit();
+      this.lectureForm.reset();
+    }).catch((error) => {
+      console.error('Error adding document:', error);
+    });
+  }
 }
