6511ee719b30532c072ad863d4bd90bb97482cf5
Rocard Fonji <fonkengrocard@gmail.com>
2024-03-05 02:13:15 +0100

    chore: install angular fire, add script to load cofigs from .env file

---
+++ dotenv.ts
@@ -0,0 +1,32 @@
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const writeFilePromisified = promisify(writeFile);
+
+const targetPath = './src/environments/environment.ts';
+
+const envConfigFile = `export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: '${process.env['PROJECT_ID']}',
+    appId: '${process.env['APP_ID']}',
+    storageBucket: '${process.env['STOREAGE_BUCKET']}',
+    apiKey: '${process.env['API_KEY']}',
+    authDomain: '${process.env['AUTH_DOMAIN']}',
+    messagingSenderId: '${process.env['MESSAGING_SENDER_ID']}',
+  },
+  filestackApiKey: '${process.env['FILESTACK_API_KEY']}',
+};
+`;
+
+(async () => {
+  try {
+    await writeFilePromisified(targetPath, envConfigFile);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+})();
