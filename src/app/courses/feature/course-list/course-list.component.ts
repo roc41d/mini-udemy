f46dbd413706c260276d82d50729951e40751bdb
Rocard Fonji <fonkengrocard@gmail.com>
2024-03-05 20:55:05 +0100

    (feat:): implement logic to get courses data from firestore and modify list ui to asynchronously display the data

--- src/app/courses/feature/course-list/course-list.component.ts
+++ src/app/courses/feature/course-list/course-list.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Course } from '../../interface/types';
-import { COURSES } from '../../data-access/dummy-data';
 import { CourseCardComponent } from '../../ui/course-card/course-card.component';
 import { ModalComponent } from '../../../shared/ui/modal.component';
 import { CreateCourseComponent } from '../../ui/create-course/create-course.component';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CourseCardComponent, ModalComponent, CreateCourseComponent],
+  imports: [
+    CourseCardComponent,
+    ModalComponent,
+    CreateCourseComponent,
+    AsyncPipe
+  ],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit{
   isCreateCourseModalOpen = false;
-  courses: Course[] = COURSES;
+  courses$!: Observable<Course[]>;
+  firestore = inject(Firestore);
+
+  ngOnInit(): void {
+    const courseRef = collection(this.firestore, 'courses');
+    this.courses$ = collectionData(courseRef) as Observable<Course[]>;
+  }
 }
